7a350a31034df8fac4105ff5c2e2d44b6f8d6b04
Prince <princeasa2005@gmail.com>
2023-10-31 22:23:02 +0000

    Taskss

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 15 == 0:
+            print("FizzBuzz", end="")
+        elif number % 3 == 0:
+            print("Fizz", end="")
+        elif number % 5 == 0:
+            print("{}".format(number), end="")
+        if (number < 100):
+            print(" ", end="")
+    print()
+
+fizzbuzz()

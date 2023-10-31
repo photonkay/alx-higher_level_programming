b23c1845b41f4ed73d986f225f0bb5ba59226d78
Prince <princeasa2005@gmail.com>
2023-10-31 20:08:47 +0000

    Task 3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for iterator in range(97, 123):
+    if iterator == 101 or iterator == 113:
+        next
+    else:
+        char = chr(iterator)
+        print("{}".format(char), end="")

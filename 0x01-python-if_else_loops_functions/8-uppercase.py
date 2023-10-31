9daa5f35b97719e35efa0ab1b9854a5be84287e4
Prince <princeasa2005@gmail.com>
2023-10-31 22:48:29 +0000

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,16 +3,10 @@
 def uppercase(s):
     result = ""
     for char in s:
-        # Check if the character is a lowercase letter
         if 'a' <= char <= 'z':
-            # Convert to uppercase using ASCII values
             uppercase_char = chr(ord(char) - ord('a') + ord('A'))
             result += uppercase_char
         else:
             result += char
-    # Print the uppercase string followed by a new line
-    print(result + "\n")
+    print("{}".format(result) + "\n")
 
-# Example usage
-input_string = "Hello, World!"
-uppercase(input_string)

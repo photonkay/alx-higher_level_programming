bb819a32bb82b9083de772502f432003976ac145
Prince <princeasa2005@gmail.com>
2023-10-31 21:12:20 +0000

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:02}, {:02}".format(i, j), end="")
+        if i != 8 or j != 9:
+            print(", ", end="")
+print()

a42db8a34d85634fb58500c4ef108675a0509d80
Prince <princeasa2005@gmail.com>
2023-11-22 08:30:11 +0000

    Instantiation, innit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This script creates a square class"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialise the dimensions of square
+
+        Args:
+            size (int): size of square
+         """
+        self.__size = size

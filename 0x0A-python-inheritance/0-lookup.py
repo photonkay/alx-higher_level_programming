c518abed732ded79e4f5586de03627e65ed9137f
Prince <princeasa2005@gmail.com>
2023-12-04 22:31:32 +0000

    Print list of attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Script to print attributes of an object"""
+
+
+def lookup(obj):
+    """function that returns a list of object attributes
+        and methods
+    """
+    return dir(obj)

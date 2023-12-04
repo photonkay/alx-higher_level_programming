ca688f8708979c41e7c7f56cefb92b305ba9d9e7
Prince <princeasa2005@gmail.com>
2023-12-04 22:54:37 +0000

    Inherited from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 
 class MyList(list):
     """This class inherits the list class"""
+    def __init__(self):
+         """initializes the object"""
+         super().__init__()
+
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
